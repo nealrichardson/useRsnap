bec1472d8f6396270bf675b7cb6fa0a26d9daf7a
Neal Richardson <neal@crunch.io>
2017-06-27 21:39:08 -0700

    Query reports by state (open/closed/all)

--- R/reports.R
+++ R/reports.R
@@ -1,20 +1,34 @@
 #' Query Your Usersnaps
 #'
+#' @param state Include "open" or "closed" reports. Default is `NULL`, which is
+#' queries both open and closed reports.
 #' @param ... query parameters to include. There's no documentation, but known
 #' parameters are (1) includeapikey, which should always be `"false"`; (2)
-#' offset; (3) limit; and (4) search, which by default is
-#' `'[{"type":"state","id":"open"}]'`, the default in the Usersnap GUI. You
+#' offset; (3) limit; and (4) search, which gets filled in with the "state"
+#' query, if provided, like `'[{"type":"state","id":"open"}]'`. You
 #' can also specify a "project" to query against a project other than your
 #' default.
 #' @return A list of report entries.
 #' @export
 #' @importFrom utils modifyList
-getReports <- function (...) {
+#' @examples
+#' \dontrun{
+#' reps <- getReports(state="open")
+#' }
+getReports <- function (state=NULL, ...) {
+    dots <- list(...)
+    if (!is.null(state)) {
+        ## NB: if there's already a "search" in ..., this will overwrite it
+        dots$search <- paste0('[{"type":"state","id":"',
+            match.arg(state, c("open", "closed")), '"}]')
+    }
     query <- modifyList(list(
         includeapikey="false",
         offset=0,
-        limit=10,
-        search='[{"type":"state","id":"open"}]'
-    ), list(...))
+        limit=10
+    ), dots)
     return(usGET(usURL("reports", ...), query=query)$data)
 }
+
+
+# Report entity: usGET(usURL(file.path("reports", ticketnr)))$data; can optionally include "search" query (I guess for next/prev, which we don't care about here)
--- tests/testthat/test-get-reports.R
+++ tests/testthat/test-get-reports.R
@@ -6,7 +6,18 @@ with_mock_API({
         expect_length(reps, 3)
     })
 
+    test_that("Get reports by state", {
+        active <- getReports(state="open")
+        expect_identical(vapply(active, function (x) x$closed, logical(1)),
+            c(FALSE, FALSE, FALSE))
+        closed <- getReports(state="closed")
+        expect_identical(vapply(closed, function (x) x$closed, logical(1)),
+            c(TRUE, TRUE))
+    })
+
     test_that("Error handling", {
+        expect_error(getReports(state="Georgia"),
+            "'arg' should be one of")
         expect_error(getReports(limit=-1),
             "Internal Server Error")
     })
